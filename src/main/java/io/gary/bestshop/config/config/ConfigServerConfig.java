69ccb36229ebb854de322d07bb114a1213d3f01b
Gary Zhang <gary.zhang@technest.es>
2018-03-28 23:16:43 +0200

    first commit

---
+++ src/main/java/io/gary/bestshop/config/config/ConfigServerConfig.java
@@ -0,0 +1,10 @@
+package io.gary.bestshop.config.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfig {
+
+}
